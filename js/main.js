cbd40b6ae1f36a4e26d75593eaecc5ed26a8d1a5
zarMarathon <zarmarathonmobisvit@gmail.com>
2020-08-20 15:19:39 +0200

    Homework 2

--- js/main.js
+++ js/main.js
@@ -1,44 +1,7 @@
-// const firstRow = 'Мама мыла раму';
-// const secondRow = 'Собака друг человека';
-
-// function getRow(firstRow, secondRow) {
-//    let counter = 0;
-//    let counter2 = 0;
-
-//    for (let i = 0; i <= firstRow; i++) {
-//       if (firstRow.charAt(i) == "а") {
-//          counter++;
-//       }
-
-//    }
-
-//    for (let i = 0; i <= secondRow; i++) {
-//       if (secondRow.charAt(i) == "а") {
-//          counter2++;
-//       }
-
-//    }
-
-//    if (counter > counter2) {
-//       console.log(firstRow);
-//    } else {
-//       console.log(secondRow);
-//    }
-// }
-
-
-
-
-// console.log(getRow(firstRow, secondRow));
-
-
 
 const firstRow = 'Мама мыла раму';
 const secondRow = 'Собака друг человека';
 
-// const search = 'а';
-
-// const indexSerch = firstRow.indexOf(search);
 let counter = 0;
 let counterSecond = 0;
 
